8bc987f24708a344aaf24dabaf7217d068147055
anishk07 <yadavanishk@gmail.com>
2021-06-12 15:00:05 +0530

    ADDED 2_arithmetic_operation.py

---
+++ 2_arithmetic_operation.py
@@ -0,0 +1,18 @@
+# @author: ANISHK YADAV
+
+"""
+Task
+The provided code stub reads two integers from STDIN, a and b. Add code to print three lines where:
+1. The first line contains the sum of the two numbers.
+2. The second line contains the difference of the two numbers (first - second).
+3. The third line contains the product of the two numbers.
+"""
+
+# CODE:
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a+b)  # sum of the two numbers
+    print(a-b)  # difference of the two numbers
+    print(a*b)  # product of the two numbers
