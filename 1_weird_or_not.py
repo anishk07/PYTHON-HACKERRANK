64f1abd735bb90d5061533dc1e19f6c5eeac908f
anishk07 <yadavanishk@gmail.com>
2021-06-12 14:52:19 +0530

    UPDATED 1_weird_or_not.py

--- 1_weird_or_not.py
+++ 1_weird_or_not.py
@@ -25,12 +25,13 @@ if __name__ == '__main__':
 def weird_or_not():
     if (n % 2) != 0:
         print("Weird")
-    elif (n % 2) == 0 in range(2, 5):
-        print("Not Weird")
-    elif (n % 2) == 0 in range(6, 20):
-        print("Weird")
     else:
-        print("Not Weird")
+        if n in range(2, 6):
+            print("Not Weird")
+        elif n in range(6, 21):
+            print("Weird")
+        else:
+            print("Not Weird")
 
 
 weird_or_not()
