27fede5d2d4ebbf8cfd1d2f21c657f7e0ce53f42
anishk07 <yadavanishk@gmail.com>
2021-06-12 16:30:36 +0530

    ADDED 6_consecutive_strings.py

---
+++ 6_consecutive_strings.py
@@ -0,0 +1,16 @@
+# @author: ANISHK YADAV
+
+"""
+The included code stub will read an integer, n, from STDIN.
+Without using any string methods, try to print the following:
+123.....n
+Note that "..." represents the consecutive values in between.
+"""
+
+# CODE:
+
+if __name__ == '__main__':
+    n = int(input())
+
+    for i in range(n):
+        print(i+1, end="")
