e2a1752694fc911bdf56f1e1f781404911a0fe85
anishk07 <yadavanishk@gmail.com>
2021-06-18 21:05:34 +0530

    README.md

---
+++ STRINGS/6_string_validators.py
@@ -0,0 +1,19 @@
+# @author: ANISHK YADAV
+
+"""
+Task
+
+You are given a string S.
+Your task is to find out if the string S contains: alphanumeric characters, alphabetical characters, digits, lowercase and uppercase characters.
+"""
+
+# CODE:
+
+if __name__ == '__main__':
+    s = input()
+
+    print(any(i.isalnum() for i in s))
+    print(any(i.isalpha() for i in s))
+    print(any(i.isdigit() for i in s))
+    print(any(i.islower() for i in s))
+    print(any(i.isupper() for i in s))
