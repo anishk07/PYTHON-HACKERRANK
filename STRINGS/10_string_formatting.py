88b29dea4d69403dfe2f87978c31176e3cb6966f
anishk07 <yadavanishk@gmail.com>
2021-06-21 23:39:31 +0530

    ADDED 10_string_formatting.py

---
+++ STRINGS/10_string_formatting.py
@@ -0,0 +1,36 @@
+# @author: ANISHK YADAV
+
+"""
+Given an integer, n, print the following values for each integer n from 1 to n:
+
+Decimal
+Octal
+Hexadecimal (capitalized)
+Binary
+Function Description
+
+Complete the print_formatted function in the editor below.
+
+print_formatted has the following parameters:
+
+int number: the maximum value to print
+"""
+
+# CODE :
+
+
+def print_formatted(number):
+    width = len(bin(number)[2:])
+    for i in range(1, number+1):
+        dec = str(i)
+        octal = oct(i)[2:]
+        hexa = (hex(i)[2:]).upper()
+        binary = bin(i)[2:]
+        print(dec.rjust(width), octal.rjust(width),
+              hexa.rjust(width), binary.rjust(width))
+        continue
+
+
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)
